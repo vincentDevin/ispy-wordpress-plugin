861106516d48b06bcd0e7eb73027f1163b0c447b
Devin Vincent <vincentdevin111@gmail.com>
2021-06-22 13:11:29 -0500

    first commit

---
+++ ispy-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: ISpy Game Plugin
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Devin Vincent
+ * Author URI: http://www.mywebsite.com
+ */
+
+	add_action( 'the_content', 'my_thank_you_text' );
+
+	function my_thank_you_text ( $content ) {
+	    return $content .= '<p>Thank you for reading!</p>';
+	}
